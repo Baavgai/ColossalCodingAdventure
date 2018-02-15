81c17eb983e50225032a5b8bcdb54810a2b495e2
Baavgai <baavgai@chaingang.org>
2018-02-14 19:33:06 -0500

    C has been a while...

--- Rooms/rooms.c
+++ Rooms/rooms.c
@@ -26,12 +26,14 @@ void showText(const char *);
 void play(State *);
 
 int main() {
+    Command cmd;
     State state;
     initState(&state);
     showText(
         "You awake on a musty smelling bed in a spartan, windowless, room."
         " You see a painting on the wall that seems to be staring at you and a closed door."
         " You feel trapped.  You don't know how you got here, but it can't be good.");
+    userInput("test", &cmd);
     while(!state.done) {
         play(&state);
     }
@@ -43,15 +45,28 @@ void showText(const char *s) {
     printf("%s\n", s);
 }
 
+#define BUFF_SIZE 500
 void userInput(const char *msg, Command *cmd) {
-    static char buffer[1000];
-    showText(msg);
-    
+    static char buffer[BUFF_SIZE], noun[BUFF_SIZE], verb[BUFF_SIZE];
+    bool done = false;
+    cmd->verb = verb; cmd->noun = noun;
 
+    showText(msg);
+    while(!done) {
+        printf("> ");
+        if(fgets(buffer, sizeof(buffer)-1, stdin)) {
+            switch(sscanf( buffer, "%s %s", verb, noun )) {
+                case 1: 
+                case 2: done = true;
+            }
+        }
+    }
+    printf("verb: \"%s\"\n", verb);
+    printf("noun: \"%s\"\n", noun);
 }
 
 void initState(State *s) {
-
+    s->done = true;
 }
 
 void playR1(State *x) {
