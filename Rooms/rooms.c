eecb0d114f8b94c42231af2448962c8a96a52804
Baavgai <brettmc@gmail.com>
2018-02-16 13:51:10 -0500

    cleaner rooms.c

--- Rooms/rooms.c
+++ Rooms/rooms.c
@@ -3,6 +3,7 @@
 #include <stdbool.h>
 #include <string.h>
 #include <stdarg.h>
+#include <ctype.h>
 
 typedef enum {
     LOC_VOID, LOC_PLAYER, 
@@ -16,10 +17,15 @@ typedef struct {
     bool r1_pic_seen;
 } State;
 
+#define COMMAND_MAX 100
 typedef struct {
-    const char *verb, *noun;
+    char full[COMMAND_MAX];
+    char *verb, *noun;
+    char buffer[COMMAND_MAX];
 } Command;
 
+char *sanitizeCommandEntered(char *);
+bool parseCommand(Command *, const char *);
 void loadUserCommand(Command *);
 void display(const char *fmt, ...);
 State *initState();
@@ -27,7 +33,6 @@ void play(State *);
 
 int main() {
     State *s = initState();
-    
     display(
         "You awake on a musty smelling bed in a spartan, windowless, room."
         " You see a painting on the wall that seems to be staring at you and a closed door."
@@ -45,57 +50,47 @@ State *initState() {
     return s;
 }
 
+#define C(x) (strcmp(c->full, x)==0)
 #define V(x) (strcmp(c->verb, x)==0)
 #define N(x) (strcmp(c->noun, x)==0)
-#define VN(v,n) (V(v) && N(n))
 
 void itemFail(const char *thing) {
     display("You don't see a \"%s\" here.", thing);
 }
 
-bool zone1(State *s, Command *c) {
+bool locR1Handler(State *s, Command *c) {
     if (s->player!=LOC_R1) { return false; }
-    if (V("look")) {
-        if(N("door")) {
-            display(s->r1_door_open
-                ? "The door is open, revealing a more spacious room beyond."
-                : "The door is very sturdy, but appears unlocked.");
-        } else if(N("")) {
-            display("You are in a small room with a bed, a creepy portrait, and %s door.",
-                s->r1_door_open ? "an open" : "a closed");
-        } else if(N("painting")||N("portrait")||N("picture")) {
-            if (s->r1_pic_seen) {
-                display("The painting stares back at you.  You feel the desire to not be seen by it.");
-            } else {
-                display("The person portrayed hard to make out."
-                    " The painting is either badly aged or actually painted out of focus."
-                    " The subject could be a grotesque man or woman or angry lawn gnome."
-                    " The only element piercingly clear are black blood shot eyes that stare back at you with malice.");
-                s->r1_pic_seen = true;
-            }
+    if(C("look door")) {
+        display(s->r1_door_open
+            ? "The door is open, revealing a more spacious room beyond."
+            : "The door is very sturdy, but appears unlocked.");
+    } else if(C("look")) {
+        display("You are in a small room with a bed, a creepy portrait, and %s door.",
+            s->r1_door_open ? "an open" : "a closed");
+    } else if(C("look painting")||C("look portrait")||C("look picture")) {
+        if (s->r1_pic_seen) {
+            display("The painting stares back at you.  You feel the desire to not be seen by it.");
         } else {
-            itemFail(c->noun);
+            display("The person portrayed hard to make out."
+                " The painting is either badly aged or actually painted out of focus."
+                " The subject could be a grotesque man or woman or angry lawn gnome."
+                " The only element piercingly clear are black blood shot eyes that stare back at you with malice.");
+            s->r1_pic_seen = true;
         }
-    } else if (V("open")) {
-        if(N("door")) {
-            if (s->r1_door_open) {
-                display("The door is already open.");
-            } else {
-                display("The door creeks open ominously.");
-                s->r1_door_open = true;
-            }
+    } else if (C("open door")) {
+        if (s->r1_door_open) {
+            display("The door is already open.");
         } else {
-            itemFail(c->noun);
+            display("The door creeks open ominously.");
+            s->r1_door_open = true;
         }
-    } else if (V("close")) {
-        if(N("door")) {
+    } else if (C("close door")) {
         display(s->r1_door_open
             ? "The door appears stuck now, it won't budge."
             : "The door still closed.");
-        } else {
-            itemFail(c->noun);
-        }
-    } else if (V("leave")||V("go")||V("exit")||VN("use","door")) {
+    } else if(V("look")||V("open")||V("close")) {
+        itemFail(c->noun);
+    } else if (V("leave")||V("go")||V("exit")||C("use door")) {
         if (s->r1_door_open) {
             display("You find yourself in another windowless room."
                 " In addition to the door you just walked through, there are two more doors, both closed."
@@ -111,42 +106,90 @@ bool zone1(State *s, Command *c) {
     return true;
 }
 
-bool zone0(State *s, Command *c) {
+// LOC_R1, LOC_R2, LOC_R3, LOC_R4, LOC_R5
+bool locR2Handler(State *s, Command *c) { return false; }
+bool locR3Handler(State *s, Command *c) { return false; }
+bool locR4Handler(State *s, Command *c) { return false; }
+bool locR5Handler(State *s, Command *c) { return false; }
+
+
+bool defaultHandler(State *s, Command *c) {
     if (V("die")) {
         display("You throw yourself at the ground.  Hard.  Ouch.  The world swirls away into darkness.");
         s->done = true;
     } else {
-        return false;
+        display("I'm confused, I don't understand \"%s\"", c->full);
     }
     return true;
 }
 
+// void (*pf[])(void) = {fna, fnb, fnc, ..., fnz};
+
 void play(State *s) {
+    static bool(*handlers[])(State *, Command *)  = {
+        locR1Handler, locR2Handler, locR3Handler, locR4Handler, locR5Handler,
+        defaultHandler
+    };
+    static const int handlerCount = sizeof(handlers) / sizeof(*handlers);
+    int i;
     Command c;
     loadUserCommand(&c);
-    if (!(zone1(s, &c) || zone0(s, &c))) {
-        if (*(c.noun) == 0) {
-            display("I'm confused, I don't understand \"%s\"", c.verb);
-        } else {
-            display("I'm confused, I don't understand \"%s %s\"", c.verb, c.noun);
+    for(i=0; i<handlerCount; i++) {
+        if (handlers[i](s, &c)) { break; }
+    }
+}
+
+
+// lib ----------
+
+char *sanitizeCommandEntered(char *cmd) {
+    bool lastSpace = true;
+    char *s = cmd;
+    char *d = cmd;
+    // for(; *s!=' '; s++);
+    for(; *s!='\0'; s++) {
+        if (*s==' ') {
+            if (!lastSpace) {
+                *d++ = ' ';
+                lastSpace = true;
+            }
+        } else if (isalpha(*s)) {
+            *d++ = tolower(*s);
+            lastSpace = false;
         }
+    }
+    if (d>cmd && *(d - 1)==' ') { --d; }
+    *d= '\0';
+    return cmd;
+}
+
+bool parseCommand(Command *cmd, const char *s) {
+    if (sanitizeCommandEntered(strcpy(cmd->full, s))[0]==0) { 
+        cmd->buffer[0] = 0;
+        cmd->noun = cmd->verb = cmd->buffer;
+        return false;
+    } else {
+        cmd->noun = cmd->verb = strcpy(cmd->buffer, cmd->full);
+        for(; *(cmd->noun)!='\0'; cmd->noun++) {
+            if (*(cmd->noun)==' ') {
+                *(cmd->noun)='\0';
+                cmd->noun++;
+                break;
+            }
+        }
+        return true;
     }
 }
 
 void loadUserCommand(Command *cmd) {
-    static char buff[1000], verb[1000], noun[1000];
+    char buffer[COMMAND_MAX];
     bool done = false;
     while(!done) {
         printf("> ");
-        if(fgets(buff, sizeof(buff)-1, stdin)) {
-            switch(sscanf( buff, "%s %s", verb, noun )) {
-                case 1: *noun = 0;
-                case 2: done = true;
-            }
+        if(fgets(buffer, COMMAND_MAX - 1, stdin)) {
+            done = parseCommand(cmd, buffer);
         }
     }
-    cmd->verb = verb;
-    cmd->noun = noun;
 }
 
 
@@ -181,3 +224,22 @@ void display(const char *fmt, ...) {
   va_end (args);
   displayWrap(buffer, 70);
 }
+
+/*
+
+    
+
+    while(sscanf( buff, "%s", word )) {
+        if (!buff!=0) { strcat(buff, " ") };
+        printf("> ");
+        if(fgets(buff, sizeof(buff)-1, stdin)) {
+            switch(sscanf( buff, "%s %s", verb, noun )) {
+                case 1: *noun = 0;
+                case 2: done = true;
+            }
+        }
+    }
+    cmd->verb = verb;
+    cmd->noun = noun;
+
+*/
